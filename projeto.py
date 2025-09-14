f002ff1fae876b79f1b280b0429318e07d959990
Julia Schiavi <juliaschiavi18@gmail.com>
2025-09-14 19:06:20 -0300

    funçoes cadastrar e listar

---
+++ projeto.py
@@ -0,0 +1,35 @@
+# Projeto: Central de Notícias - Futebol Feminino
+# Integrantes: Julia Schiavi, Leonardo Grosskof, Thayna Lopes, Sofia Bomeny
+
+
+noticias = []  
+id_atual = 1   
+
+
+def cadastrar():
+    global id_atual
+    titulo = input("Título: ")
+    resumo = input("Resumo: ")
+    categoria = input("Categoria (clube/campeonato/jogadora): ")
+    noticia = {
+        "id": id_atual,
+        "titulo": titulo,
+        "resumo": resumo,
+        "categoria": categoria
+    }
+    noticias.append(noticia)
+    id_atual += 1
+    print("✅ Notícia cadastrada!\n")
+
+
+def listar():
+    if not noticias:
+        print("⚠ Nenhuma notícia cadastrada.\n")
+    else:
+        print("\n=== Todas as Notícias ===")
+        for n in noticias:
+            print(f"ID: {n['id']} | {n['categoria'].upper()}")
+            print(f"Título: {n['titulo']}")
+            print(f"Resumo: {n['resumo']}\n")
+
+
